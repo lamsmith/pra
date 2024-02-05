5474f6273371707b1b89dc2b6cda5da3d8b1e08d
unknown <nuhuibrahim04@gmail.com>
2024-02-05 12:42:05 +0100

    ib

---
+++ context/Transport.cs
@@ -0,0 +1,6 @@
+class TransportConetext
+{
+    List <Admin> adminManager = new List<Admin>();
+    static List<AvailableRouteAndPrice> availableRouteAndPrice = new List<AvailableRouteAndPrice>();
+    static List<Customer> Customers  = new List<Customer>();   
+}
